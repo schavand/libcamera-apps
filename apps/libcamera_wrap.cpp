0e6c8ad926e4f53aa172649fa16acffc00a5977b
schavand <sylvain.chavand@free.fr>
2022-05-04 10:14:27 +0200

    wrapper additions : options set, stream and streaminfo seed functions ... debug

--- apps/libcamera_wrap.cpp
+++ apps/libcamera_wrap.cpp
@@ -15,90 +15,177 @@
 
 extern "C" {
 	
-	libcameraAppHandle createLibcameraApp()
+	libcameraAppHandle *createLibcameraApp()
 	{
 
-		return new LibcameraApp;
+		return reinterpret_cast<libcameraAppHandle*>(new LibcameraApp());
 	}
 
-	void freeLibcameraApp(libcameraAppHandle h)
+	void freeLibcameraApp(libcameraAppHandle *h)
 	{
-		delete (LibcameraApp *) h;
+		delete reinterpret_cast<LibcameraApp*>(h);
 	}
 
-	void OpenCamera(libcameraAppHandle h)
+	void OpenCamera(libcameraAppHandle *h)
 	{
-		((LibcameraApp *)h)->OpenCamera();
+		reinterpret_cast<LibcameraApp*>(h)->OpenCamera();
 	}
 
-	void CloseCamera(libcameraAppHandle h)
+	libcameraOptions *GetOptions(libcameraAppHandle *h)
+	{
+		return reinterpret_cast<libcameraOptions*>(reinterpret_cast<LibcameraApp*>(h)->GetOptions());
+	}
+
+	bool ParseOptions(libcameraOptions *o, int argc, char *argv[])
+	{
+		return reinterpret_cast<Options *>(o)->Parse(argc, argv);
+	}
+
+	void CloseCamera(libcameraAppHandle *h)
 	{
 		((LibcameraApp *)h)->CloseCamera();
 	}
 
-	void ConfigureViewfinder(libcameraAppHandle h)
+	void ConfigureViewfinder(libcameraAppHandle *h)
 	{
 		((LibcameraApp *)h)->ConfigureViewfinder();
 	}
 	
-	void ConfigureStill(libcameraAppHandle h, unsigned int flags)
+	void ConfigureStill(libcameraAppHandle *h, unsigned int flags)
 	{
 		((LibcameraApp *)h)->ConfigureStill(flags);
 	}
 
-	void ConfigureVideo(libcameraAppHandle h, unsigned int flags)
+	void ConfigureVideo(libcameraAppHandle *h, unsigned int flags)
 	{
 		((LibcameraApp *)h)->ConfigureVideo(flags);
 	}
 
-	void Teardown(libcameraAppHandle h)
+	void Teardown(libcameraAppHandle *h)
 	{
 		((LibcameraApp *)h)->Teardown();
 	}
 
-	void StartCamera(libcameraAppHandle h)
+	void StartCamera(libcameraAppHandle *h)
 	{
 		((LibcameraApp *)h)->StartCamera();
 	}
-	void StopCamera(libcameraAppHandle h)
+	void StopCamera(libcameraAppHandle *h)
 	{
 		((LibcameraApp *)h)->StopCamera();
 	}
 
-	void Wait(libcameraAppHandle h, libcameraAppMsg m)
+/*	libcameraAppMsg *libcameraWait(libcameraAppHandle *h)
 	{
-		*((LibcameraApp::Msg *)m) = ((LibcameraApp *)h)->Wait();
+		static LibcameraApp::Msg msg = ((LibcameraApp *)h)->Wait();
+		return reinterpret_cast<libcameraAppMsg*>(&msg);
+	}
+*/
+	void libcameraWait(libcameraAppHandle *h, libcameraAppMsg *m)
+	{
+		*reinterpret_cast<LibcameraApp::Msg*>(m) = ((LibcameraApp *)h)->Wait();
 	}
 
-	libcameraAppMsg createLibcameraAppMsg()
-	{
-		void *m = malloc(sizeof(LibcameraApp::Msg));
-		return m;
-	}
-
-	void freeLibcameraAppMsg(libcameraAppMsg m)
-	{
-		free(m);
-	}
-
-	int getLibcameraMessageType(libcameraAppMsg m)
+	int getLibcameraMessageType(libcameraAppMsg *m)
 	{
 		return  static_cast<int>(((LibcameraApp::Msg *)m)->type);
 	}
 
-	void *getLibcameraMessagePayload(libcameraAppMsg m)
+	void *getLibcameraMessagePayload(libcameraAppMsg *m)
 	{
 		return  static_cast<void*>(&((LibcameraApp::Msg *)m)->payload);
 	}
 
-	void ShowPreview(libcameraAppHandle h, void* completed_request, void *stream)
+	void ShowPreview(libcameraAppHandle *h, libcameraCompletedRequestPtr* completed_request, libcameraStream *stream)
 	{
-		CompletedRequestPtr *c = static_cast<CompletedRequestPtr *>(completed_request);
-		((LibcameraApp *) h)->ShowPreview(*c, static_cast<libcamera::Stream *>(stream));
+		((LibcameraApp *) h)->ShowPreview(*reinterpret_cast<CompletedRequestPtr *>(completed_request), reinterpret_cast<libcamera::Stream *>(stream));
 	}
 
-	void *getCompletedRequestPtr(libcameraAppMsg m)
+	libcameraCompletedRequestPtr *getCompletedRequestPtr(libcameraAppMsg *m)
 	{
-		return static_cast<void*>(&std::get<CompletedRequestPtr>(((LibcameraApp::Msg *)m)->payload));
+		return reinterpret_cast<libcameraCompletedRequestPtr*>(&(((LibcameraApp::Msg *)m)->payload));
+//		return reinterpret_cast<libcameraCompletedRequestPtr*>(&std::get<CompletedRequestPtr>(((LibcameraApp::Msg *)m)->payload));
+	}
+
+	libcameraStream *getViewfinderStream(libcameraAppHandle *h, libcameraStreamInfo *info)
+	{
+		return (libcameraStream *)(reinterpret_cast<LibcameraApp*>(h)->ViewfinderStream(reinterpret_cast<StreamInfo *>(info)));
+	}
+	
+	libcameraStream *getStillStream(libcameraAppHandle *h, libcameraStreamInfo *info)
+	{
+		return (libcameraStream *)(reinterpret_cast<LibcameraApp *>(h)->StillStream(reinterpret_cast<StreamInfo *>(info)));
+	}
+	
+	libcameraStream *getRawStream(libcameraAppHandle *h, libcameraStreamInfo *info)
+	{
+		return (libcameraStream *)(reinterpret_cast<LibcameraApp *>(h)->RawStream(reinterpret_cast<StreamInfo *>(info)));
+	}
+
+	libcameraStream *getVideoStream(libcameraAppHandle *h, libcameraStreamInfo *info)
+	{
+		return (libcameraStream *)(reinterpret_cast<LibcameraApp *>(h)->VideoStream(reinterpret_cast<StreamInfo *>(info)));
+	}
+	
+	libcameraStream *LoresStream(libcameraAppHandle *h, libcameraStreamInfo *info)
+	{
+		return (libcameraStream *)(reinterpret_cast<LibcameraApp *>(h)->LoresStream(reinterpret_cast<StreamInfo *>(info)));
+	}
+
+    libcameraStreamInfo *createStreamInfo()
+	{
+		return reinterpret_cast<libcameraStreamInfo *>(new StreamInfo());
+	}
+
+    uint32_t getStreamInfoWidth(libcameraStreamInfo *info)
+	{
+		return reinterpret_cast<StreamInfo*>(info)->width;
+	}
+
+    uint32_t getStreamInfoHeight(libcameraStreamInfo *info)
+	{
+		return reinterpret_cast<StreamInfo*>(info)->height;
+	}    
+
+    uint32_t getStreamInfoStride(libcameraStreamInfo *info)
+	{
+		return reinterpret_cast<StreamInfo*>(info)->stride;
+	}    
+
+    uint32_t getStreamInfoPixel(libcameraStreamInfo *info)
+	{
+		return reinterpret_cast<StreamInfo*>(info)->pixel_format.fourcc();
+	}    
+
+    libcameraAppMsg *createAppMsg()
+	{
+		return reinterpret_cast<libcameraAppMsg *>(new LibcameraApp::Msg({}));
+	}
+
+	uint8_t *getFrame(libcameraAppHandle *h, libcameraAppMsg *msg, libcameraStream *s)
+	{ 
+//		const std::vector<libcamera::Span<uint8_t>> mem = app.Mmap((*reinterpret_cast<CompletedRequestPtr*>(payload))->buffers[stream])
+		CompletedRequestPtr &_payload = std::get<CompletedRequestPtr>(reinterpret_cast<LibcameraApp::Msg *>(msg)->payload);
+		const std::vector<libcamera::Span<uint8_t>> mem = reinterpret_cast<LibcameraApp *>(h)->Mmap(_payload->buffers[reinterpret_cast<libcamera::Stream*>(s)]);
+		return (uint8_t *)(mem[0].data());
+	}
+
+
+	void optionSetValue(libcameraOptions *o, OptionList ol, void *value)
+	{
+		switch (ol)
+		{
+			case awb_gain_r:
+				reinterpret_cast<Options*>(o)->awb_gain_r = *(float *)value;
+				break;
+			case awb_gain_b:
+				reinterpret_cast<Options*>(o)->awb_gain_b = *(float *)value;
+				break;
+		}
+	}
+
+    void freeStreamInfo(libcameraStreamInfo *s)
+	{
+		delete reinterpret_cast<StreamInfo*>(s);
 	}
 }
--- apps/libcamera_wrap.h
+++ apps/libcamera_wrap.h
@@ -2,26 +2,55 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    typedef void * libcameraAppHandle;
-    typedef void * libcameraAppMsg;
 
-    libcameraAppHandle createLibcameraApp();
-    void OpenCamera(libcameraAppHandle h);
-    void CloseCamera(libcameraAppHandle h);
-    void ConfigureViewfinder(libcameraAppHandle h);
-    void ConfigureStill(libcameraAppHandle h, unsigned int flags);
-    void ConfigureVideo(libcameraAppHandle h, unsigned int flags);
-    void Teardown(libcameraAppHandle h);
-    void StartCamera(libcameraAppHandle h);
-    void StopCamera(libcameraAppHandle h);
-    void Wait(libcameraAppHandle h, libcameraAppMsg m);
-    libcameraAppMsg createLibcameraAppMsg();
-    void freeLibcameraAppMsg(libcameraAppMsg m);
-    int getLibcameraMessageType(libcameraAppMsg m);
-    void *getLibcameraMessagePayload(libcameraAppMsg m);
-    void ShowPreview(libcameraAppHandle h, void* completed_request, void *stream);
-    void *getCompletedRequestPtr(libcameraAppMsg m);
-    void    freeLibcameraApp(libcameraAppHandle h);
+#include <stdint.h>
+    enum libcameraMsgType
+	{
+		RequestComplete,
+		Quit
+	};
+
+   	typedef enum OptionList {awb_gain_r,awb_gain_b} OptionList;
+
+    typedef struct libcameraAppHandle libcameraAppHandle;
+    typedef struct libcameraAppMsg libcameraAppMsg;
+    typedef struct libcameraOptions libcameraOptions;
+    typedef struct libcameraStream libcameraStream;
+    typedef struct libcameraStreamInfo libcameraStreamInfo; 
+    typedef struct libcameraCompletedRequestPtr libcameraCompletedRequestPtr;
+
+    libcameraAppHandle *createLibcameraApp();
+    void OpenCamera(libcameraAppHandle *h);
+    void CloseCamera(libcameraAppHandle *h);
+    libcameraOptions *GetOptions(libcameraAppHandle *h);
+    bool ParseOptions(libcameraOptions *o, int argc, char *argv[]);
+    void ConfigureViewfinder(libcameraAppHandle *h);
+    void ConfigureStill(libcameraAppHandle *h, unsigned int flags);
+    void ConfigureVideo(libcameraAppHandle *h, unsigned int flags);
+    void Teardown(libcameraAppHandle *h);
+    void StartCamera(libcameraAppHandle *h);
+    void StopCamera(libcameraAppHandle *h);
+    void libcameraWait(libcameraAppHandle *h, libcameraAppMsg *m);
+    libcameraAppMsg *createAppMsg();
+    int getLibcameraMessageType(libcameraAppMsg *m);
+    void *getLibcameraMessagePayload(libcameraAppMsg *m);
+    void ShowPreview(libcameraAppHandle *h, libcameraCompletedRequestPtr *completed_request, libcameraStream *stream);
+    uint8_t *getFrame(libcameraAppHandle *h, libcameraAppMsg *msg, libcameraStream *s);
+    libcameraCompletedRequestPtr *getCompletedRequestPtr(libcameraAppMsg *m);
+    libcameraStream *getViewfinderStream(libcameraAppHandle *h, libcameraStreamInfo *info);
+    libcameraStream *getRawStream(libcameraAppHandle *h, libcameraStreamInfo *info);
+    libcameraStream *getVideoStream(libcameraAppHandle *h, libcameraStreamInfo *info);
+    libcameraStream *getLoresStream(libcameraAppHandle *h, libcameraStreamInfo *info);
+    libcameraStream *getStillStream(libcameraAppHandle *h, libcameraStreamInfo *info);
+    libcameraStreamInfo *createStreamInfo();
+    uint32_t getStreamInfoWidth(libcameraStreamInfo *info);
+    uint32_t getStreamInfoHeight(libcameraStreamInfo *info);
+    uint32_t getStreamInfoStride(libcameraStreamInfo *info);
+    uint32_t getStreamInfoPixel(libcameraStreamInfo *info);
+    void freeStreamInfo(libcameraStreamInfo *s);
+    void freeLibcameraApp(libcameraAppHandle *h);
+    void optionSetValue(libcameraOptions *o, OptionList ol, void *value);
+
 #ifdef __cplusplus
 }
 #endif
